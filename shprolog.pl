a198b08f50204096c169b9c94e14a57e90108301
Arturo Garcia <r2d293@live.com.mx>
2016-12-07 09:03:37 -0800

    reglas para verificar el super heroe

--- shprolog.pl
+++ shprolog.pl
@@ -24,6 +24,51 @@ write('************************'),
 nl,
 nl,
 
+
+
+/*reglas para saber el super heroe/*
+
+sera(batman) :- batman, !.
+
+sera(superman) :- superman, !.
+
+sera(mujer_maravilla) :- mujer_maravilla, !.
+
+sera(detective_marciano) :- detective_marciano, !.
+
+sera(aquaman) :- aquaman, !.
+
+sera(flecha_verde) :- flecha_verde, !.
+
+
+/*metodo para verificar el super heroe*/
+batman :- verificar(logo_de_murcielago).
+
+superman :- verificar(s_de_logo).
+
+detective_marciano :- verificar(piel_verde).
+
+aquaman :- verificar(respira_bajo_el_agua).
+
+mujer_maravilla :- verificar(es_mujer).
+
+flecha verde :- verificar(usa_flechas).
+
+
+logo_de_murcielago :- verificar(su_logo_es_un_murcielago).
+
+s_de_logo :- verificar(su_logo_es_una_s).
+
+es_mujer :- verificar(es_mujer).
+
+respira_bajo_el_agua :- verificar(respira_bajo_el_agua).
+
+usa_flechas :- verificar(dispara_flechas).
+
+piel_verde :- verificar(piel_verde).
+
+
+
 /*como saber si adivino el super heroe*/
 write('¿He adivinado? (s/n) '),
 read(Respuesta),
